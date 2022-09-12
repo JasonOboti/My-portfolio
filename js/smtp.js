e553cef4e951d2ec3736730cf4d5ccab4ffd619f
JasonOboti <89173793+JasonOboti@users.noreply.github.com>
2022-09-12 15:58:51 +0100

    update

--- js/smtp.js
+++ js/smtp.js
@@ -1,18 +1,16 @@
 function sendEmail() {
-    const firstName = document.getElementById("fname")
-    const lastName = document.getElementById("lname")
-    const email = document.getElementById("email")
-    const subject = document.getElementById("subject")
-    const message = document.getElementById("message")
+  const firstName = document.getElementById("fname");
+  const lastName = document.getElementById("lname");
+  const email = document.getElementById("email");
+  const subject = document.getElementById("subject");
+  const message = document.getElementById("message");
 
-    Email.send({
-        SecureToken : "b9b2cfe3-c9e8-4efb-9e3b-e782a0fc31d8",
-        To : 'jasonoboti@gmail.com',
-        From : 'obotijason@gmail.com',
-        Subject : "My Port-folio Contact Form",
-        Body : `Firstname: ${firstName.value} <br/> Lastname: ${lastName.value} <br/> Email: ${email.value} <br/> Subject: ${subject.value}
-        <br/>Message: ${message.value}`
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
+  Email.send({
+    SecureToken: "b9b2cfe3-c9e8-4efb-9e3b-e782a0fc31d8",
+    To: "jasonoboti@gmail.com",
+    From: "obotijason@gmail.com",
+    Subject: "My Port-folio Contact Form",
+    Body: `Firstname: ${firstName.value} <br/> Lastname: ${lastName.value} <br/> Email: ${email.value} <br/> Subject: ${subject.value}
+        <br/>Message: ${message.value}`,
+  }).then((message) => alert("Message Sent Successfully"));
 }
